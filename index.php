283fc329afe0e3a54b2813ce4a0f1221194e1ebd
rea-jin <reazack.souki@gmail.com>
2022-12-16 17:44:00 +0900

    azure devops用

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        h1{
+            color:red;
+        }
+        h2{
+            background-color: aqua;
+        }
+    </style>
+    <?php
+    echo '<div>hello</div>';
+    echo '<h1>this site is azure devops test page</h1>';
+    echo '<h2>devops starter made select simple php </h2>';
+    echo '<h3>web site is made by simple php</h3>';
+    echo '<p>thats all</p>';
+    ?>
+
+</body>
+</html>
+
